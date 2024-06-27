7fe5eb2f7685fe90635fa8b5f35ed07e4f480a86
Romina Cardozo <romis.cardozo@gmail.com>
2024-06-27 00:20:15 -0300

    correcion url

--- nasa_image_gallery/urls.py
+++ nasa_image_gallery/urls.py
@@ -1,35 +1,38 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index_page, name='index-page'),
-    path('home/', views.home, name='home'),
-    path('home/register', views.home, name='register'),
-    # Rutas de inicio de sesión
-    path('login/', views.login, name='login'),
-    path('home/login/', views.login, name='login'),
-    path('buscar/login/', views.login, name='login'),
-
-    # Rutas de cierre de sesión
-    path('logout/', views.exit, name='logout'),
-    path('home/logout/', views.exit, name='logout'),
-    path('buscar/logout/', views.exit, name='logout'),
-    path('favourites/logout/', views.exit, name='logout'),
-    path('home/register/logout/', views.exit, name='logout'), 
-
-    # Ruta de búsqueda
-    path('buscar/', views.search, name='buscar'),
-
-    # Rutas de favoritos
-    path('favourites/', views.getAllFavouritesByUser, name='favoritos'),
-    path('favourites/add/', views.saveFavourite, name='agregar-favorito'),
-    path('favourites/delete/', views.deleteFavourite, name='borrar-favorito'),
-
-    # Rutas de registro
-    path('register/', views.register, name='register'),
-    path('login/register/', views.register, name='register'),
-    path('home/register/', views.register, name='register'),
-    path('register/home', views.register, name='register'),
-    # Ruta de salida
-    path('exit/', views.exit, name='exit'),
+from django.urls import path 
+from . import views 
+ 
+urlpatterns = [ 
+    path('', views.index_page, name='index-page'), 
+    path('home/', views.home, name='home'), 
+    
+    # Rutas de inicio de sesión 
+    path('login/', views.login, name='login'), 
+    path('home/login/', views.login, name='login'), 
+    path('buscar/login/', views.login, name='login'), 
+ 
+    # Rutas de cierre de sesión 
+    path('logout/', views.exit, name='logout'), 
+    path('home/logout/', views.exit, name='logout'), 
+    path('buscar/logout/', views.exit, name='logout'), 
+    path('favourites/logout/', views.exit, name='logout'), 
+    path('home/register/logout/', views.exit, name='logout'),  
+ 
+    # Ruta de búsqueda 
+    path('buscar/', views.search, name='buscar'), 
+ 
+    # Rutas de favoritos 
+    path('favourites/', views.getAllFavouritesByUser, name='favoritos'), 
+    path('favourites/add/', views.saveFavourite, name='agregar-favorito'), 
+    path('favourites/delete/', views.deleteFavourite, name='borrar-favorito'), 
+ 
+    # Rutas de registro 
+    path('register/', views.register, name='register'), 
+    path('login/register/', views.register, name='register'), 
+    path('home/register/', views.register, name='register'), 
+    path('register/home/', views.register, name='register'), 
+    path('buscar/register/',views.register, name='register'), 
+    path('home/register',views.register, name='register'), 
+    path('', views.register, name='register'), 
+    # Ruta de salida 
+    path('exit/', views.exit, name='exit'), 
 ]
